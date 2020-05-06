33051a9e8524d8f68a6fd0130f8a4bc5fbb68a72
Tarandeep Singh <kritrim@airdetarandeep.home>
2020-05-06 19:09:07 +0200

    'op'

--- src/App.svelte
+++ src/App.svelte
@@ -1,48 +1,91 @@
 <script>
-  import { onMount } from "svelte";
-  export let date;
+  let files;
+  let bening;
+  let malignant;
 
-  onMount(async () => {
-    const res = await fetch("/api/date");
-    const newDate = await res.text();
-    date = newDate;
-  });
+  const url = "https://skin-cancer-api.herokuapp.com/predict";
+
+  const draw = () => {
+    var img = document.getElementById("image");
+    document.getElementById("button").disabled = false;
+    img.src = URL.createObjectURL(files[0]);
+    bening = null;
+    malignant = null;
+  };
+
+  const predict = () => {
+    document.getElementById("button").disabled = true;
+
+    let formData = new FormData();
+    formData.append("file", files[0]);
+
+    fetch(url, {
+      method: "POST",
+      body: formData
+    })
+      .then(res => res.json())
+      .then(res => {
+        bening = (res.bening * 100).toFixed(2);
+        malignant = (res.malignant * 100).toFixed(2);
+        console.log(res);
+      });
+  };
 </script>
 
-<main>
-  <h1>Svelte + Node.js API</h1>
-  <h2>
-    Deployed with
-    <a href="https://vercel.com/docs" target="_blank" rel="noreferrer noopener">
-      Vercel
-    </a>
-    !
-  </h2>
-  <p>
-    <a
-      href="https://github.com/zeit/now/tree/master/examples/svelte"
-      target="_blank"
-      rel="noreferrer noopener">
-      This project
-    </a>
-    is a
-    <a href="https://svelte.dev/">Svelte</a>
-    app with three directories,
-    <code>/public</code>
-    for static assets,
-    <code>/src</code>
-    for components and content, and
-    <code>/api</code>
-    which contains a serverless
-    <a href="https://nodejs.org/en/">Node.js</a>
-    function. See
-    <a href="/api/date">
-      <code>api/date</code>
-      for the Date API with Node.js
-    </a>
-    .
-  </p>
-  <br />
-  <h2>The date according to Node.js is:</h2>
-  <p>{date ? date : 'Loading date...'}</p>
-</main>
+<style>
+  div {
+    max-width: 600px;
+    margin: 0 auto;
+    display: grid;
+    align-items: center;
+    justify-content: center;
+    grid-gap: 20px;
+  }
+
+  img {
+    max-width: 90%;
+    max-height: 200px;
+    margin: 0 auto;
+  }
+
+  h1,
+  p {
+    margin: 0;
+    text-align: center;
+  }
+
+  input,
+  p {
+    box-sizing: border-box;
+    padding: 0px 10px;
+  }
+
+  button {
+    width: 100px;
+    margin: 0 auto;
+    background-color: rgb(46, 231, 169);
+    border: none;
+  }
+</style>
+
+<div>
+  <h1>Skin Cancer</h1>
+  <p>Select an image of a skin mole to get a prediction.</p>
+  <input
+    id="fileinput"
+    type="file"
+    accept="image/*"
+    bind:files
+    on:change={draw} />
+
+  <img src="" alt="" id="image" />
+
+  {#if files}
+    <button on:click={predict} id="button">PREDICT</button>
+  {/if}
+
+  {#if bening}
+    <p>Bening: {bening} %</p>
+    <p>Malignant: {malignant} %</p>
+  {/if}
+</div>
